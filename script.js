75ff727d1b770bf6c33033f194adc47a8f80e631
Juan <joaopbg06@gmail.com>
2023-08-10 09:48:49 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 let button = document.querySelector('#enviar')
 
 button.addEventListener('click', function (){
-    let numeroCartao = document.querySelector('#numero_cartao').value
-    let textoResultado = document.querySelector('#resultado')
-    console.log(numeroCartao)
+    let numeroCartao = document.querySelector('#numero_cartao').value;
+    let textoResultado = document.querySelector('#resultado');
 
     if (numeroCartao >= 1000 && numeroCartao < 2000){
-        textoResultado.innerHTML = 'parabens voce tem acesso a area restrita'
+        textoResultado.innerHTML = 'Parabens você tem acesso a area restrita'
         textoResultado.style.color = 'green'
     }
+
     else if(numeroCartao >= 2000 && numeroCartao < 3000){
-        textoResultado.innerHTML = 'acesso apenas a algumas areas restritas'
+        textoResultado.innerHTML = 'Acesso apenas a algumas areas restritas'
         textoResultado.style.color = 'white'
     }
+
     else if(numeroCartao == 0){
-        textoResultado.innerHTML = 'informe o numero do cartao por favor'
+        textoResultado.innerHTML = 'Por favor informe o número do cartão'
+        textoResultado.style.color = 'gray'
     }
+
     else{
-        textoResultado.innerHTML = 'voce nao tem acesso'
+        textoResultado.innerHTML = 'Sem acesso'
         textoResultado.style.color = 'red'
     }
 
